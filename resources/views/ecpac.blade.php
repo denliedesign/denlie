d7bdf2be2018670b467d8ea39c06044c7cbe0779
denliedesign <customdenlie@gmail.com>
2021-08-10 20:35:30 -0500

    ecpac aug 15 min

--- resources/views/ecpac.blade.php
+++ resources/views/ecpac.blade.php
@@ -12,7 +12,7 @@
             <div class="pb-5">
                 <p class="text-center mb-0">1 Hour Included Updates</p>
                 <div class="bar rounded d-flex justify-content-start">
-{{--                    <div class="progress-bars hrs-25 text-center rounded included-bars">15 min</div>--}}
+                    <div class="progress-bars hrs-25 text-center rounded included-bars">15 min</div>
                     <div class="text-center rounded included-bars"></div>
                 </div>
             </div>
@@ -40,7 +40,7 @@
                     <div class="col my-3"><h4>May</h4> 45 min</div>
                     <div class="col my-3"><h4>Jun</h4> 0</div>
                     <div class="col my-3"><h4>Jul</h4> 15 min</div>
-                    <div class="col my-3"><h4>Aug</h4> 0</div>
+                    <div class="col my-3"><h4>Aug</h4> 15 min</div>
                     <div class="col my-3"><h4>Sep</h4> -</div>
                     <div class="col my-3"><h4>Oct</h4> -</div>
                     <div class="col my-3"><h4>Nov</h4> -</div>
